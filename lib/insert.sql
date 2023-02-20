a183bd762080ef8e9485ea110e00758f71141890
treva556 <marktreva92@gmail.com>
2023-02-20 12:38:40 +0300

    done

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears VALUES (1,"Mr. Chocolate", 22, "M", "brown", "calm", 1);
-INSERT INTO bears VALUES (2,"Rowdy", 20, "M", "black", "melancholy", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 2, "M", "black", "melancholy", 1);
 INSERT INTO bears VALUES (3,"Tabitha", 16, "F", "dark blue", "supine", 0);
 INSERT INTO bears VALUES (4,"Sargeant Brown", 39, "M", "green", "sanguine", 1);
-INSERT INTO bears VALUES (5,"Melissa", 53, "F", "grey", "goofy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 5, "F", "grey", "goofy", 0);
 INSERT INTO bears VALUES (6,"Grinch", 2, "M", "purple", "choleric", 1);
 INSERT INTO bears VALUES (7,"Wendy", 62, "F", "white", "happy", 0);
 INSERT INTO bears VALUES (8,null, 20, "M", "red", "moody", 0);
